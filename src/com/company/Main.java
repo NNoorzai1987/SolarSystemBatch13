088076296dbef11c95c930c44a1d8ed916f6a689
Wellstudy1985 <Nnoorzai.001@gmail.com>
2022-08-27 17:03:21 -0700

    Solar System ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        SolarSystem inception=new SolarSystem();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+
+        System.out.println("The basic structure is ready");
 
     }
 }
